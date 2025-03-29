d655aaadf9c42ec233decf8a62e1b6e687fbc4d5
Daniel Barcelos <daniel.barcelos@2mind.com.br>
2025-03-29 13:31:24 -0300

    Update composer.json and run script

--- run.sh
+++ run.sh
@@ -2,12 +2,17 @@
 
 set -e
 
+# Resolve root project path
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
+
+BIN="${PROJECT_ROOT}/vendor/bin"
+
 # Colors
 GREEN="\033[0;32m"
 YELLOW="\033[1;33m"
 BLUE="\033[1;34m"
 RED="\033[0;31m"
-NC="\033[0m" # No Color
+NC="\033[0m"
 
 divider() {
     echo -e "${YELLOW}--------------------------------------------------${NC}"
@@ -20,29 +25,29 @@ section() {
 }
 
 finish() {
-    echo -e "${GREEN}✅ $1 completed successfully!${NC}\n"
+    echo -e "${GREEN}✅ $1 completed successfully!${NC}\\n"
 }
 
 fail() {
-    echo -e "${RED}❌ $1 failed!${NC}\n"
+    echo -e "${RED}❌ $1 failed!${NC}\\n"
 }
 
-echo -e "${GREEN}🚀 Starting full code quality analysis...${NC}\n"
+echo -e "${GREEN}🚀 Starting full code quality analysis...${NC}\\n"
 
 # Laravel Pint
 section "Running Laravel Pint"
-./vendor/bin/pint --config review/pint.json
-if [ $? -eq 0 ]; then finish "Laravel Pint"; else fail "Laravel Pint"; fi
+"${BIN}/pint" --config "${BASH_SOURCE%/*}/src/pint.json" || fail "Laravel Pint"
+finish "Laravel Pint"
 
 # PHP CodeSniffer
 section "Running PHP CodeSniffer"
-./vendor/bin/phpcs --colors -s --standard=review/phpcs.xml
-if [ $? -eq 0 ]; then finish "PHP CodeSniffer"; else fail "PHP CodeSniffer"; fi
+"${BIN}/phpcs" --colors -s --standard="${BASH_SOURCE%/*}/src/phpcs.xml" || fail "PHP CodeSniffer"
+finish "PHP CodeSniffer"
 
 # PHPArkitect
 section "Running PHPArkitect"
-./vendor/bin/phparkitect check --config=review/arkitect.php
-if [ $? -eq 0 ]; then finish "PHPArkitect"; else fail "PHPArkitect"; fi
+"${BIN}/phparkitect" check --config="${BASH_SOURCE%/*}/src/arkitect.php" || fail "PHPArkitect"
+finish "PHPArkitect"
 
 divider
 echo -e "${GREEN}🎉 Code quality analysis completed successfully!${NC}"
